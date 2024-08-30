da1b74139a5ece25b6110573c7264acf1790b5c6
Agrawal-Vansh <vanshaggrawal1@gmail.com>
2024-08-30 18:15:08 +0530

    Frontend Improvement

--- Frontend/src/pages/RegisterPage.jsx
+++ Frontend/src/pages/RegisterPage.jsx
@@ -4,7 +4,6 @@ import RegisterForm from '../components/RegisterForm';
 const RegisterPage = () => {
   return (
     <div>
-      <h2>Register</h2>
       <RegisterForm /> {/* Use the RegisterForm component */}
     </div>
   );
