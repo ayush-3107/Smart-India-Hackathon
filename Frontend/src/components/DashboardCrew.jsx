d6e1b8dea8f9da3a3352956cc225b9a08ffac06d
Mohak-Mittal <mohakmittal012@gmail.com>
2024-08-31 19:08:38 +0530

    Added pages

--- Frontend/src/components/DashboardCrew.jsx
+++ Frontend/src/components/DashboardCrew.jsx
@@ -6,55 +6,11 @@ import Header from './Header/Header';
 import Footer from './Footer/Footer';
 
 const DashboardCrew = () => {
-  // const [data, setData] = useState([]);
-  // const [loading, setLoading] = useState(true);
-  // const [error, setError] = useState(null);
-
-  // useEffect(() => {
-  //   // Fetch the JSON file when the component mounts
-  //   fetch('/mockCrewMembers.json')
-  //     .then(response => {
-  //       if (!response.ok) {
-  //         throw new Error('Network response was not ok');
-  //       }
-  //       return response.json();
-  //     })
-  //     .then(jsonData => {
-  //       setData(jsonData);
-  //       setLoading(false);
-  //     })
-  //     .catch(error => {
-  //       console.error('Error fetching JSON data:', error);
-  //       setError(error);
-  //       setLoading(false);
-  //     });
-  // }, []);
-
-  // if (loading) return <p>Loading...</p>;
-  // if (error) return <p>Error loading data: {error.message}</p>;
-
   return (
     <>
-    {/* <div>
-      <table>
-        <thead>
-          <tr>
-            {data[0] && Object.keys(data[0]).map((key) => (
-              <th key={key}>{key}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((row, index) => (
-            <tr key={index}>
-              {Object.values(row).map((value, i) => (
-                <td key={i}>{value}</td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div> */}
+      <div className="shadow-lg rounded-lg flex flex-col items-center justify-center">
+        <img src="ScheduLine_Tagline_img.png" className="bg-cover bg-center" alt="Banner" />
+      </div>
     </>
   );
 };
