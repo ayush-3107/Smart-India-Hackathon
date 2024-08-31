273627741400b7041204f61c4054000bc8666c1c
Tanisha020 <tanisha2318@gmail.com>
2024-08-31 16:03:02 +0530

    Styled about and header

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 function Header() {
     return (
         <header className="shadow sticky z-50 top-0">
-            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+            <nav className="bg-[#F1F8E8] border-gray-200 px-4 lg:px-6 py-2.5"> {/* Updated background color */}
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
                         <img
-                            src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                            src="/logo.png"  
                             className="mr-3 h-12"
                             alt="Logo"
                         />
@@ -16,13 +16,13 @@ function Header() {
                     <div className="flex items-center lg:order-2">
                         <Link
                             to="/"
-                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                            className="text-gray-800 hover:bg-[#D8EFD3] focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Log in
                         </Link>
                         <Link
                             to="/register"
-                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                            className="text-white bg-[#55AD9B] hover:bg-[#95D2B3] focus:ring-4 focus:ring-[#D8EFD3] font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
                         >
                             Get started
                         </Link>
@@ -36,20 +36,17 @@ function Header() {
                                 <Link
                                     to="/"
                                     className={
-                                        `block py-2 pr-4 pl-3 duration-200
-                                border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-[#D8EFD3] lg:hover:bg-transparent lg:border-0 hover:text-[#55AD9B] lg:p-0`
                                     }
-                                // isActive is used to add different css on the link which is active
                                 >
-
                                     Home
                                 </Link>
                             </li>
                             <li>
                                 <Link
-                                    to="/about"    // add the location in to
+                                    to="/about"
                                     className={
-                                        `block py-2 pr-4 pl-3 duration-200  border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-[#D8EFD3] lg:hover:bg-transparent lg:border-0 hover:text-[#55AD9B] lg:p-0`
                                     }
                                 >
                                     About
@@ -59,20 +56,18 @@ function Header() {
                                 <Link
                                     to="/contact"
                                     className={
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 hover:bg-[#D8EFD3] lg:hover:bg-transparent lg:border-0 hover:text-[#55AD9B] lg:p-0`
                                     }
                                 >
                                     Contact
                                 </Link>
                             </li>
-
                         </ul>
                     </div>
                 </div>
             </nav>
         </header>
-    )
+    );
 }
-
 
 export default Header;
